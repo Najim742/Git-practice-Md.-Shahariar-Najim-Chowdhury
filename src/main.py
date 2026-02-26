77005c6a3d01c6b4a0470bb54357c113948d4910
Najim742 <najimchowdhury742@gmail.com>
2026-02-26 22:41:43 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
 import datetime
+from utils import  add, subtract
 
 print("Md. Shahariar Najim Chowdhury")
 print("Today's date:", datetime.date.today())
+
+#Call from utils.py
+
+print("Addition:", add(5, 3))
+print("Subtraction:", subtract(5, 3))
